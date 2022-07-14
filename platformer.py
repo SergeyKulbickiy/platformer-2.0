716c39be4eb4de5ec7d7a2343bc44142d2012cca
Sergey <sergey.kulbuckiy@gmail.com>
2022-07-14 19:59:34 +0300

    Add enemies

--- platformer.py
+++ platformer.py
@@ -142,6 +142,9 @@ class World():
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                if tile == 3:
+                    blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+                    blob_group.add(blob)
                 col_count += 1
             row_count += 1
 
@@ -149,6 +152,24 @@ class World():
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
             pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/blob.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
 
 
 world_data = [
@@ -175,6 +196,9 @@ world_data = [
 ]
 
 player = Player(100, screen_height - 130)
+
+blob_group = pygame.sprite.Group()
+
 world = World(world_data)
 
 run = True
@@ -186,6 +210,9 @@ while run:
     screen.blit(sun_img, (100, 100))
 
     world.draw()
+
+    blob_group.update()
+    blob_group.draw(screen)
 
     player.update()
 
