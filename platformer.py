0e7ed7f9e6dc655ca6e026dc2eaf8f07064f36d6
Sergey <sergey.kulbuckiy@gmail.com>
2022-07-14 18:26:16 +0300

    Create Player

--- platformer.py
+++ platformer.py
@@ -17,10 +17,50 @@ sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
 
 
-def draw_grid():
-    for line in range(0, 20):
-        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+class Player():
+    def __init__(self, x, y):
+        img = pygame.image.load('img/guy1.png')
+        self.image = pygame.transform.scale(img, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+
+    def update(self):
+        dx = 0
+        dy = 0
+
+        # get keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+
+        # add gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        # check for collision
+
+        # update player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            dy = 0
+
+        # draw player onto screen
+        screen.blit(self.image, self.rect)
 
 
 class World():
@@ -80,6 +120,7 @@ world_data = [
     [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+player = Player(100, screen_height - 130)
 world = World(world_data)
 
 run = True
@@ -90,7 +131,7 @@ while run:
 
     world.draw()
 
-    draw_grid()
+    player.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
